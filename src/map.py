8e60986b2938a6e74d76fe18e5995215b5d201ca
Angell Pimienta's <angelefrain1510@gmail.com>
2025-04-11 12:11:23 -0500

    update maps

--- src/map.py
+++ src/map.py
@@ -1,6 +1,21 @@
 import osmnx as ox
 
 place_name = "Medellín, Colombia"
-graph = ox.graph_from_place(place_name, network_type="drive")
+G = ox.graph_from_place(place_name, network_type="drive")
 
-ox.plot_graph(graph)
+# ox.plot_graph(graph)
+
+ox.plot_graph(G, node_size=1, node_color="red")
+
+def style_unvisited_edge(edge):        
+    G.edges[edge]["color"] = "#d36206"
+    G.edges[edge]["alpha"] = 0.2
+    G.edges[edge]["linewidth"] = 0.5
+
+def style_visited_edge(edge):
+    G.edges[edge]["color"] = "#d36206"
+    G.edges[edge]["alpha"] = 1
+    G.edges[edge]["linewidth"] = 1
+
+for edges in G.edges:
+    style_visited_edge(edges)
